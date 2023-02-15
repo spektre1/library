acb6a6d1b6e389636e56a972a1787c03e1f14309
Danielle MacDonald <spektre1@gmail.com>
2023-02-14 20:36:03 -0600

    Stubbed things in and made flask shell run

--- install.ps1
+++ install.ps1
@@ -5,3 +5,5 @@ pip install flask connexion
 # PYTHONPATH=/src:/src/src
 
 $env:PYTHONPATH += ';D:\usr\dmacdonald\source\repos\library\api'
+
+$env:FLASK_APP = 'D:\usr\dmacdonald\source\repos\library\api\app.py:app.app'
