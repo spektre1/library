397f32a6b4eb87c00b7ff592ca4866f25174392d
Danielle MacDonald <spektre1@gmail.com>
2023-02-20 16:30:26 -0600

    scraping gutenberg

---
+++ api/db/loadGutenberg.py
@@ -0,0 +1,16 @@
+"""Using the gutenberg100.json, load into db through API."""
+
+import requests
+import json
+
+props = ['title', 'author', 'text_url', 'cover_url']
+
+with open('gutenberg100.json', 'r') as f:
+    books = json.load(f)
+
+for book in books:
+    resp = requests.post(
+        url='localhost:5000/api/v1/book/add',
+        json={prop:getattr(book, prop) for prop in props})
+    if resp.status !=200:
+        print(f'oh noes! Resp status {resp.status}: {resp.body}')
--- api/gutenberg/scraper.py
+++ api/gutenberg/scraper.py
@@ -1,10 +1,16 @@
+"""This scrapes the top 100 books of the last 30 days, and writes a json
+file with their biblographic record."""
 from gutenberg.api import Gutenberg
+import json
 
 G = Gutenberg()
 
 titles = G.getTopTitles()
-# book = G.getBookInfo(titles[0][3])
 
 bookInfo = []
 for title in titles:
     bookInfo.append(G.getBookInfo(title[3]))
+
+# Write bookInfo to file:
+with open('gutenberg100.json', 'w') as f:
+    f.write(json.dumps(bookInfo, sort_keys=True, indent=2))
