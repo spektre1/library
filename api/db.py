4096f84d8be09fbfb3c32e15ecbe61191f89a08c
Danielle MacDonald <spektre1@gmail.com>
2023-03-09 16:18:29 -0600

    working many to many map? Testing

--- api/db.py
+++ api/db.py
@@ -1,5 +1,8 @@
 """Initialize the database object. This is eventually attached to the Flask
 application after all the models are constructed."""
 from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy import create_engine
+# engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
 
+#db = SQLAlchemy(engine=engine)
 db = SQLAlchemy()
--- api/models.py
+++ api/models.py
@@ -1,12 +1,35 @@
 """SQLAlchemy Database models."""
 
 import sqlalchemy as sa
+from sqlalchemy import Integer, String, ForeignKey, Column, Table
+from typing import List, Optional
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from db import db
 
-class Book(db.Model):
-    """Represents a biblographic record for a book."""
-    id        = sa.Column(sa.Integer, primary_key=True)
-    title     = sa.Column(sa.String, nullable=False)
-    author    = sa.Column(sa.String)
-    text_url  = sa.Column(sa.String, nullable=False)
-    cover_url = sa.Column(sa.String)
+class Base(DeclarativeBase):
+    pass
+
+# Represents the relationship of authors to a book.
+book_authors = Table(
+    'book_authors',
+    Base.metadata,
+    Column('author_id', ForeignKey('authors.id')),
+    Column('book_id', ForeignKey('books.id'))
+    )
+
+
+class Author(Base):
+    """Represents an author."""
+    __tablename__ = "authors"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+
+
+class Book(Base):
+    """Represents a book."""
+    __tablename__ = "books"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str]
+    author: Mapped[List["Author"]] = relationship('Author', secondary=book_authors, backref='books')
+    text_url: Mapped[str]
+    cover_url: Mapped[Optional[str]]
