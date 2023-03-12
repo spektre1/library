6e59b1a9c28c35b4026b36f4a68c76507c6212f1
Danielle MacDonald <spektre1@gmail.com>
2023-03-12 18:47:41 -0500

    models load now

--- api/db/loadGutenberg.py
+++ api/db/loadGutenberg.py
@@ -2,15 +2,24 @@
 
 import requests
 import json
+from db import db
+from models import Book, Author
 
-props = ['title', 'author', 'text_url', 'cover_url']
+props = ['title', 'text_url', 'cover_url']
 
 with open('gutenberg100.json', 'r') as f:
     books = json.load(f)
 
+# for book in books:
+#     resp = requests.post(
+#         url='http://localhost:5000/api/v1/book/add',
+#         json={prop:book[prop] for prop in props})
+#     if resp.status_code !=200:
+#         print(f'oh noes! Resp status {resp.status}: {resp.body}')
+# 
+
 for book in books:
-    resp = requests.post(
-        url='http://localhost:5000/api/v1/book/add',
-        json={prop:book[prop] for prop in props})
-    if resp.status_code !=200:
-        print(f'oh noes! Resp status {resp.status}: {resp.body}')
+    b = Book(**{k: book[k] for k in props})
+    b.author.append(Author(name=book['author']))
+    db.session.add(b)
+    db.session.commit()
